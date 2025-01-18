06c6bca257fa0087119f02460d16df298bca8423
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-01-18 15:46:21 +0200

    commit

--- client/src/pages/SarbProdEmploy.jsx
+++ client/src/pages/SarbProdEmploy.jsx
@@ -12,18 +12,18 @@ import {
 
 ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
-// Get base URL based on environment
-const baseUrl =
-  import.meta.env.VITE_ENV === "development"
-    ? import.meta.env.VITE_DEV_BASE_URL
-    : import.meta.env.VITE_PROD_BASE_URL;
-
-axios.defaults.baseURL = baseUrl;
-
 const SarbProdEmploy = () => {
   const [response, setResponse] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  // Get base URL based on environment
+  const baseUrl =
+    import.meta.env.VITE_ENV === "development"
+      ? import.meta.env.VITE_DEV_BASE_URL
+      : import.meta.env.VITE_PROD_BASE_URL;
+
+  axios.defaults.baseURL = baseUrl;
 
   useEffect(() => {
     const fetchData = async () => {
