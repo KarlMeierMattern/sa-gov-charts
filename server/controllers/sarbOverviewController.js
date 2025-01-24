8d664dbdafeeab781a6acf576dd3580d8ee68acf
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-01-24 19:09:24 +0200

    commit

--- server/controllers/sarbOverviewController.js
+++ server/controllers/sarbOverviewController.js
@@ -5,7 +5,7 @@ import sarbOverviewScraper from "../scraping/sarbOverviewScraper.js";
 
 const postSarbOverview = async (req, res) => {};
 
-// CHANGE 1
+// CHANGE 2
 const getSarbOverview = async (req, res) => {
   try {
     const url = process.env.SARB_OVERVIEW;
