08ac70eec1b8219a0663a7ba4b4563a68a14597d
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-01-24 19:10:32 +0200

    Revert "1st commit"
    This reverts commit 7c3c5120898c74a959b660b61a64a6cdf7c8eefe.

--- server/controllers/sarbOverviewController.js
+++ server/controllers/sarbOverviewController.js
@@ -5,7 +5,7 @@ import sarbOverviewScraper from "../scraping/sarbOverviewScraper.js";
 
 const postSarbOverview = async (req, res) => {};
 
-// CHANGE 2
+// CHANGE 1
 const getSarbOverview = async (req, res) => {
   try {
     const url = process.env.SARB_OVERVIEW;
