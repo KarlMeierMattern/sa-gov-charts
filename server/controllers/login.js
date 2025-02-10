df461e680efa12f25a2595c5860ffe8c284c2daa
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-02-10 19:59:12 +0200

    commit

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,4 +1,5 @@
 import { CustomError } from "../errors/custom-error.js";
+import jwt from "jsonwebtoken";
 
 const signup = async (req, res) => {
   res.status(200).send("Signup route");
@@ -6,11 +7,23 @@ const signup = async (req, res) => {
 
 // Ensure that only requests with JWT can access the dashboard
 const login = async (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    throw new CustomError("Please provide username & password", 300);
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new CustomError("Please provide username & password", 300);
+    }
+
+    // payload, secret, options
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res
+      .status(200)
+      .json({ msg: "Success", data: { username, password }, token });
+  } catch (error) {
+    console.log(error);
   }
-  res.status(200).json({ msg: "Success", data: { username, password } });
 };
 
 export { signup, login };
