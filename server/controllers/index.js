8dd9b3055cbf507af57634a1dadc576d7d378753
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-05-10 20:41:25 +0200

    commit

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,7 +3,7 @@ import { getSarbAllData } from "./sarbAllController.js";
 import { getSarbOtherIndicatorsController } from "./sarbOtherIndicatorsController.js";
 import { getSarbRepoData } from "./sarbRepoController.js";
 import { getSarbRepoTimelineData } from "./sarbRepoTimelineController.js";
-import { getSarbFxTimelineData } from "./sarbFXTimelineController.js";
+import { getSarbFxTimelineData } from "./sarbFxTimelineController.js";
 import { getSarbRealGdpTimelineData } from "./sarbRealGdpTimelineController.js";
 import { getTest } from "./testController.js";
 export {
