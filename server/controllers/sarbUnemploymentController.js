fd62fc6ba84c73c7b614c15f9fdbf85caf288d4e
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-05-18 14:48:24 +0200

    commit

--- server/controllers/sarbUnemploymentController.js
+++ server/controllers/sarbUnemploymentController.js
@@ -1,11 +1,13 @@
-import unemploymentScraper from "../scraping/unemploymentScraper.js";
+import { SarbUnemploymentModel } from "../model/index.js";
 
-export const getUnemployment = async (req, res) => {
+const getUnemployment = async (req, res) => {
   try {
-    const data = await unemploymentScraper(process.env.SARB_UNEMPLOYMENT);
+    const data = await SarbUnemploymentModel.find();
     res.status(200).json(data);
   } catch (error) {
     console.error("Error in getUnemployment:", error);
     res.status(500).json({ error: "Failed to retrieve unemployment data" });
   }
 };
+
+export { getUnemployment };
