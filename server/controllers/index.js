2e2ec9a314b8c28b438dc7117314d34f939fc89e
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-06-10 20:35:47 +0200

    commit

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -14,7 +14,6 @@ import { getSarbEuroTimelineData } from "./sarbEuroTimelineController.js";
 import { getTest } from "./testController.js";
 import { getUnemployment } from "./sarbUnemploymentController.js";
 import { getUnemploymentTimelineData } from "./sarbUnemploymentTimelineController.js";
-import { getWebsiteData } from "./websiteController.js";
 export {
   getJseIndex,
   getSarbAllData,
@@ -32,5 +31,4 @@ export {
   getTest,
   getUnemployment,
   getUnemploymentTimelineData,
-  getWebsiteData,
 };
